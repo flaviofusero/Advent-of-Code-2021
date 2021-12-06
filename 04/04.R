2377878b4c11192e440b6448ed1b7396d476922f
unknown <flavio.fusero@gmail.com>
2021-12-06 13:46:48 +0100

    initial commit

---
+++ 04/04.R
@@ -0,0 +1,96 @@
+library(magrittr)
+library(stringr)
+library(readr)
+
+parse_data <- function(path) {  
+  input <- read_file(path) %>% 
+    str_split('\r\n\r\n') %>% 
+    unlist %>% 
+    str_extract_all('[:digit:]+') %>% 
+    lapply(as.numeric)
+  
+  nums <- input[[1]]
+  
+  ncol <- input[[2]] %>% 
+    length %>% 
+    sqrt
+  
+  tables <- lapply(input[-1], matrix, ncol = ncol)
+  marked <- rep(
+    list(matrix(FALSE, ncol = ncol)), 
+    length(input[-1]))
+  
+  return(list(nums = nums, tables = tables, marked = marked))
+}
+
+path = 'input.txt'
+dat <- parse_data(path)
+
+extract_number <- function(iter, dat) {
+  num <- dat$nums[iter]
+  num_index <- lapply(dat$tables, function(x) which(x == num, arr.ind = T))
+  
+  for (i in 1:length(dat$tables)) {
+    if (length(num_index[[i]])) dat$marked[[i]][num_index[[i]][1], num_index[[i]][2]] <- TRUE
+  }
+  
+  return(dat)         
+}
+
+check_victory <- function(dat) {
+  ncols <- ncol(dat$tables[[1]])
+  table_wins <- lapply(dat$marked, function(x) any(c(colSums(x), rowSums(x)) == ncols)) %>% 
+    unlist
+  
+  if (any(table_wins)) {
+    return(which(table_wins == TRUE)[1])
+  }
+  
+  return(0)
+}
+
+get_winning_score <- function(dat, iter, winning_table_index) {
+  winning_table <- dat$tables[[winning_table_index]]
+  unmarked <- which(dat$marked[[winning_table_index]] == FALSE, arr.ind = TRUE)
+  return(sum(winning_table[unmarked]) * dat$nums[iter])
+}
+
+# part 1
+
+ans1 <- function(dat) {
+  iter = 0
+  
+  while (!check_victory(dat)) {
+    iter <- iter + 1
+    dat <- extract_number(iter = iter, dat = dat)
+  }
+  
+  get_winning_score(dat = dat, iter = iter, winning_table_index = check_victory(dat))
+}
+
+ans1(dat)
+
+# part 2
+
+ans2 <- function(dat) {
+  iter = 0
+  
+  while(!(length(dat$tables) == 1 & check_victory(dat))) {
+    winning_index <- check_victory(dat)
+    
+    while (!winning_index) {
+      iter <- iter + 1
+      dat <- extract_number(iter = iter, dat = dat)
+      winning_index <- check_victory(dat)
+    }
+    
+    if (length(dat$tables) > 1) {
+      dat$tables <- dat$tables[-winning_index]
+      dat$marked <- dat$marked[-winning_index]
+    }
+  }
+  
+  get_winning_score(dat = dat, iter = iter, winning_table_index = 1)
+}
+
+ans2(dat)
---
+++ 06/06.R
@@ -0,0 +1,43 @@
+library(magrittr)
+library(stringr)
+library(readr)
+
+path = 'input.txt'
+
+parse_data <- function(path) {
+  dat <- readLines(path)
+  str_extract_all(dat, '[:digit:]') %>% 
+    lapply(as.numeric) %>% 
+    unlist
+}
+
+make_initial_state <- function(dat) {
+  counters <- table(dat)
+  for (i in 0:8) if (is.na(counters[paste0(i)])) counters[paste0(i)] <- 0
+  counters[order(factor(names(counters)))]
+}
+
+evolve_state <- function(counters) {
+  zeroes <- counters[1]
+  counters <- lead(counters)
+  counters[7] <- counters[7] + zeroes
+  counters[9] <- zeroes
+  
+  counters
+}
+
+elapse_days <- function(counters, iters) {
+  for (i in 1:iters) {
+    counters <- evolve_state(counters)
+  }
+  
+  counters
+}
+
+dat <- parse_data(path)
+ans <- function(dat, days) {
+  elapse_days(make_initial_state(dat), days) %>% sum
+}
+
+ans(dat, 80)
+ans(dat, 256)
