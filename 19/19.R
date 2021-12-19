d30f9ba56d02712aa8659731c5abba632e2fd5ee
unknown <flavio.fusero@gmail.com>
2021-12-19 16:32:34 +0100

    initial commit

--- 19/19.R
+++ 19/19.R
@@ -29,11 +29,11 @@ parse_data <- function(path) {
 }
 
 # Returns the positive-oriented rotation matrices of R^3
-make_bases <- function() {
+make_rotations <- function() {
   bases <- list()
   m <- expand.grid(c(0, 1, -1), c(0, 1, -1), c(0, 1, -1), c(0, 1, -1), c(0, 1, -1), 
                    c(0, 1, -1), c(0, 1, -1), c(0, 1, -1), c(0, 1, -1))
-  for (we in 1:nrow(m)) {
+  for (i in 1:nrow(m)) {
     base <- matrix(as.numeric(m[i, ]), 3)
     if (det(base) == 1 & all( rowSums(abs(base)) == 1) & all(colSums(abs(base)) == 1)) {
       bases[[length(bases) + 1]] <- base
@@ -49,40 +49,40 @@ make_bases <- function() {
 nn <- function(x) sqrt(sum(x * x))
 
 get_common_beacons <- function(reports, i, j) {
-  dwe <- apply(reports[[i]], 2, function(x) apply(reports[[i]], 2, function(y) nn(x - y)))
+  di <- apply(reports[[i]], 2, function(x) apply(reports[[i]], 2, function(y) nn(x - y)))
   dj <- apply(reports[[j]], 2, function(x) apply(reports[[j]], 2, function(y) nn(x - y)))
-  common_dwe <- (dwe > 0) & (dwe %in% dj)
+  common_di <- (di > 0) & (di %in% dj)
   common_dj <- (dj > 0) & (dj %in% di)
   
   if(sum(common_di) >= 132) { 
     # 132 = 12 * 11 ways in which you can chose ordered pairs of beacons
-    bwe <- reports[[i]][, intersect(which(common_di, arr.ind = TRUE)[, 1], 
+    bi <- reports[[i]][, intersect(which(common_di, arr.ind = TRUE)[, 1], 
                                    which(rowSums(common_di) >= 11))]
     bj <- reports[[j]][, intersect(which(common_dj, arr.ind = TRUE)[, 1],
                                    which(rowSums(common_dj) >= 11))]
-    return(list(bwe = bi, bj = bj))
+    return(list(bi = bi, bj = bj))
   }
 }
 
-# If the reports use the same coordinate base, we expect the pairwise offsets in x, y and z coordinates of the
+# If the reports use the same coordinate base, i expect the pairwise offsets in x, y and z coordinates of the
 # beacons to overlap in all spots.
-# If this doesn't happen, we rotate bj (i.e. change the base of bj) and try again.
+# If this doesn't happen, i rotate bj (i.e. change the base of bj) and try again.
 
-bj_to_bwe <- function(beacons, bases) {
-  bwe <- beacons$bi
+bj_to_bi <- function(beacons, bases) {
+  bi <- beacons$bi
   bj <- beacons$bj
   
   if(is.null(bi) | is.null(bj)) {
     return(NULL)
   }
   
-  bwe <- t(t(bi)[order(t(bi)[, 1], t(bi)[, 2], t(bi)[, 3]) ,])
+  bi <- t(t(bi)[order(t(bi)[, 1], t(bi)[, 2], t(bi)[, 3]) ,])
   for (base in bases) {
     bj_ornt <- base %*% bj
     bj_ornt <- t(t(bj_ornt)[order(t(bj_ornt)[, 1], t(bj_ornt)[, 2], t(bj_ornt)[, 3]) ,])
     
     if (identical(t(apply(bi, 1, diff)), t(apply(bj_ornt, 1, diff)))) {
-      offset <- (bwe - bj_ornt)[, 1]
+      offset <- (bi - bj_ornt)[, 1]
       return(list(base = base, offset = offset))
     }
   }
@@ -98,7 +98,7 @@ orient_sea <- function(reports, bases) {
       for (j in to_orient) {
         beacons_ij <- get_common_beacons(reports, i, j)
         if (length(beacons_ij)) {
-          bjwe <- bj_to_bi(beacons_ij, bases)
+          bji <- bj_to_bi(beacons_ij, bases)
           reports[[j]] <- bji[['base']] %*% reports[[j]] + bji[['offset']]
           sonars <- rbind(sonars, unlist(bji[['offset']]))
           oriented <- c(oriented, j)
@@ -112,7 +112,7 @@ orient_sea <- function(reports, bases) {
 }
 
 sea <- parse_data(path) %>% 
-  orient_sea(bases = make_bases())
+  orient_sea(bases = make_rotations())
 
 # part 1
 Reduce(cbind, sea[['reports']]) %>% 
